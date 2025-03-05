516dfe4b7041dfc6e15599b382ca5df6bc2cf6ab
muhammadzaky <nateice16@gmail.com>
2025-03-05 10:20:40 +0700

    test for halluc 3

--- onnx_inference.py
+++ onnx_inference.py
@@ -409,7 +409,7 @@ if __name__ == "__main__":
 
     # Llama configuration remains the same.
     llama_config = {
-        'temperature': 0.001,
+        'temperature': 1,
         'topk': 1,
         'max': 1000,
         'poolsize': 39,
