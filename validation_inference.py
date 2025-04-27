333824c1600d4022ab9f025287f5c321a175f293
muhammadzaky <nateice16@gmail.com>
2025-04-27 17:05:07 +0700

    fix

--- validation_inference.py
+++ validation_inference.py
@@ -23,6 +23,37 @@ import gc
 import csv
 import json
 
+EVALUATION_SUBJECTS = [
+    "abstract_algebra",
+    "anatomy",
+    "astronomy",
+    "business_ethics",
+    "clinical_knowledge",
+    "college_biology",
+    "college_chemistry",
+    "college_computer_science",
+    "college_mathematics",
+    "college_medicine",
+    "college_physics",
+    "computer_security",
+    "conceptual_physics",
+    "econometrics",
+    "electrical_engineering",
+    "elementary_mathematics",
+    "formal_logic",
+    "global_facts",
+    "high_school_biology",
+    "high_school_chemistry",
+    "high_school_computer_science",
+    "high_school_european_history",
+    "high_school_geography",
+    "high_school_government_and_politics",
+    "high_school_macroeconomics",
+    "high_school_mathematics",
+    "high_school_microeconomics",
+    "high_school_physics",
+    "high_school_psychology"
+]
 def analyze_path(model, start_pattern, target_config):
     # Build consumer map lazily and only explore relevant paths
     consumers = defaultdict(list)
@@ -449,18 +480,10 @@ def load_mmlu_dataset():
     "high_school_microeconomics",
     "high_school_physics",
     "high_school_psychology"])
-        test_dataset = load_dataset("cais/mmlu", "miscellaneous", split="test")
-        test_dataset = test_dataset.filter(lambda x: x['subject'] in ["high_school_biology",
-    "high_school_chemistry",
-    "high_school_computer_science",
-    "high_school_european_history",
-    "high_school_geography",
-    "high_school_government_and_politics",
-    "high_school_macroeconomics",
-    "high_school_mathematics",
-    "high_school_microeconomics",
-    "high_school_physics",
-    "high_school_psychology"])
+        test_dataset = load_dataset("cais/mmlu", "all", split="test")
+        test_dataset = test_dataset.filter(lambda x: x['subject'] in EVALUATION_SUBJECTS)
+        dev_dataset = load_dataset("cais/mmlu", "all", split="dev")
+        dev_dataset = test_dataset.filter(lambda x: x['subject'] in EVALUATION_SUBJECTS)
         dev_list = [ex for ex in dev_dataset]
         test_list = [ex for ex in test_dataset]
         subjects = sorted(list(set(ex['subject'] for ex in test_list)))
