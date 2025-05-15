9abcfa470b7273d273a27b7804c6a7d08d8f2105
muhammadzaky <nateice16@gmail.com>
2025-05-15 08:32:06 +0700

    bbb

--- demo_inference.py
+++ demo_inference.py
@@ -912,8 +912,8 @@ def are_logits_equal(golden_logits, faulty_logits):
 
 def create_demo_prompt():
     """Create the demonstration prompt with one-shot example"""
-    one_shot = "\"Flash\" estimates of GDP are: \nA. subject to revision. \nB. do not require revision. \nC. available after a thirty-day lag. \nD. both A and C Answer: A"
-    problem = "(1+i)^10 = \nA. 1\nB. i\nC. 32\nD. 32i"
+    one_shot = "c in Z_3 such that Z_3[x]/(x^2 + c) is a field.\nA. 0\nB. 1\nC. 2\nD. 3\nAnswer: B\n\n"
+    problem = "Question: \"Flash\" estimates of GDP are: \nA. subject to revision. \nB. do not require revision. \nC. available after a thirty-day lag. \nD. both A and C\nAnswer:"
     return one_shot + problem
 
 def run_fault_injection_demo():
@@ -923,8 +923,8 @@ def run_fault_injection_demo():
     llama_config = {
         'temperature': 0.001,
         'topp': 0.1,
-        'max': 300,
-        'poolsize': 44,
+        'max': 50,
+        'poolsize': 40,
         'fp16': True,
         'precision': 'int8',
         'onnxdir': 'decoders/7B16',
