40cdf900e63e73d38e2710f9d2300b759cba4819
muhammadzaky <nateice16@gmail.com>
2026-04-15 14:54:44 +0700

    chores: upddating onnxsim compatibility

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 pip install -r requirements.txt
+pip install -r tools/requirements.txt
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SCRIPT_DIR/llama"
